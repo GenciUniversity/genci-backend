fd1be5f83ce5ef55cc38c6cab9f4ab521059eeb4
amit <ak3264114@gmail.com>
2023-08-02 23:45:05 +0530

    signup user api created

---
+++ server/model/user.model.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+var userSchema = new mongoose.Schema({
+  fname: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  lname: {
+    type: String,
+    trim: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+    trim: true,
+  },
+  nationality: {
+    type: String,
+    trim: true,
+  },
+  age: {
+    type: Number,
+    trim: true,
+  },
+  gender: {
+    type: String,
+    trim: true,
+  },
+  countryCode: {
+    type: String,
+    trim: true,
+  },
+  contactNumber: {
+    type: Number,
+    trim: true,
+  },
+  password: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+});
+const Userdb = mongoose.model("userdb", userSchema);
+
+module.exports = Userdb;
